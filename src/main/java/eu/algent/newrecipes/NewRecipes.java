214f35f61f232b22de2e4dff3e55bafcaedbeaaa
socram8888 <socram8888@gmail.com>
2013-04-08 16:45:17 +0200

    Fixed seamless double stone slab for 1.5.1

---
+++ src/main/java/eu/algent/newrecipes/NewRecipes.java
@@ -0,0 +1,18 @@
+package eu.algent.newrecipes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NewRecipes extends JavaPlugin {
+
+    public void onEnable() {
+        // save default config.yml if it is missing.
+        saveDefaultConfig();
+        // Init
+        new Recipes(this);
+        getLogger().info(getName() + " has been enabled.");
+    }
+
+    public void onDisable() {
+        getLogger().info(getName() + " has been disabled.");
+    }
+}
